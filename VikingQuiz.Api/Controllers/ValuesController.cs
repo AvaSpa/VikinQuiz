412e85b1924359fd426d85b77238a5bb6796a763
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-07-17 14:50:53 +0300

    Commit test 2

--- VikingQuiz.Api/Controllers/ValuesController.cs
+++ VikingQuiz.Api/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace VikingQuiz.Api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value3" };
         }
 
         // GET api/values/5
