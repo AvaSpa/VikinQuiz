3034c58ccc20a14878f4a8fd2bb24105084e1d1e
Silviu Gherman <silviu.gherman@wirtek.com>
2018-07-17 11:32:02 +0300

    Commit Test

--- VikingQuiz.Api/Controllers/ValuesController.cs
+++ VikingQuiz.Api/Controllers/ValuesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VikingQuiz.Api.Controllers
+     // hello world
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
