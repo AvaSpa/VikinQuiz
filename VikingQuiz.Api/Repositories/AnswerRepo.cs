9575effff9f8a5338ed257f82e7ea6a67e51d0c9
WIRTEK\monica.andronic <andronic.monica07@yahoo.com>
2018-07-18 15:19:13 +0300

    Answer + Player Repo

---
+++ VikingQuiz.Api/Repositories/AnswerRepo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VikingQuiz.Api.Models;
+
+namespace VikingQuiz.Api.Repositories
+{
+    public class AnswerRepo
+    {
+        private VikinQuizContext ctx = new VikinQuizContext();
+
+        public AnswerRepo(VikinQuizContext context)
+        {
+            this.ctx = context;
+        }
+
+        public void AddAnswer(Answer answer)
+        {
+            ctx.Answer.Add(answer);
+            ctx.SaveChanges();
+        }
+
+        public void DeleteAnswer(int id)
+        {
+            Answer ans = ctx.Answer.Find(id);
+            if (ans != null)
+            {
+                ctx.Answer.Remove(ans);
+                ctx.SaveChanges();
+            }        
+        }
+
+        public void UpdateAnswer(Answer ans)
+        {
+            var existingAnswer = ctx.Answer.Find(ans.Id);
+            if (existingAnswer != null)
+            {
+                existingAnswer.Text = ans.Text;
+                existingAnswer.QuestionId = ans.QuestionId;
+                existingAnswer.QuestionId = ans.QuestionId;
+                ctx.SaveChanges();
+            }    
+        }
+
+       
+        public IQueryable<Answer> GetAllAnswers()
+        {
+            return ctx.Answer;
+        }
+
+        public Answer GetAnswerById(int id)
+        {
+            return ctx.Answer.Find(id);
+        }
+
+        public Answer GetAnswerByText(string text)
+        {
+            return ctx.Answer.FirstOrDefault(d => (d.Text == text));
+        }
+
+    }
