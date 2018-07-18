b856a083f78a2a973b501e9ad6bed60b3514222e
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-07-18 10:29:21 +0300

    Added query to create table for Quizzes, Questions, QuizzQuestion, Games

--- VikingQuiz.Api/DB Scripts/Create.sql
+++ VikingQuiz.Api/DB Scripts/Create.sql
@@ -5,3 +5,28 @@
 	Pass nvarchar(100),
 	PictureURL nvarchar(max)
 );
+CREATE TABLE Quizzes
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Title VARCHAR(100) NOT NULL,
+	PictureURL VARCHAR(max) NOT NULL,
+	UserId INT REFERENCES Users(Id)
+)
+CREATE TABLE Questions
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Text VARCHAR(200) NOT NULL,
+	CorrectAnsId INT NOT NULL
+)
+CREATE TABLE QuizzQuestion
+(
+	QuizzId INT REFERENCES Quizzes(Id),
+	QuestionId INT REFERENCES Questions(Id),
+	Primary Key(QuizzId, QuestionId)
+)
+CREATE TABLE Games
+(
+	Id INT Primary Key IDENTITY,
+	QuizId INT REFERENCES Quizzes(Id),
+	GameDate Date NOT NULL
+)
