b41bfc8ccee0d017bcba0a43213ac4413f8c201c
WIRTEK\louisa.vasies <louisavasies74@gmail.com>
2018-07-18 14:51:31 +0300

    Game Repo

---
+++ VikingQuiz.Api/Repositories/GameRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VikingQuiz.Api.Models;
+
+namespace VikingQuiz.Api.Repositories
+{
+    public class GameRepo
+    {
+        private VikinQuiz ctx;
+
+        public GameRepo(VikinQuiz ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(Game g)
+        {
+            ctx.Add(g);
+            ctx.SaveChanges();
+        }
+
+        public void Update(Game g)
+        {
+            Game gg = ctx.Game.Find(g.Id);
+            if (gg != null)
+            {
+                gg.QuizId = g.QuizId;
+                gg.GameDate = g.GameDate;
+                ctx.SaveChanges();
+            }
+            else
+                throw new Exception("Game not found!");
+        }
+
+        public void Delete(int id)
+        {
+            Game g = ctx.Game.Find(id);
+            if (g != null)
+            {
+                ctx.Game.Remove(g);
+                ctx.SaveChanges();
+            }
+            else
+                throw new Exception("Game not found!");
+        }
+
+        public List<Game> GetAll()
+        {
+            return ctx.Game.ToList();
+        }
+    }
+}
