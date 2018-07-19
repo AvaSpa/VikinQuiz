06a09674bc7429110f32e9a52badd6faabefdeb2
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-07-19 10:49:29 +0300

    Add userMapper

---
+++ VikingQuiz.Api/Mappers/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VikingQuiz.Api.Models;
+using VikingQuiz.Api.ViewModels;
+using VikingQuiz.Api.Mappers;
+
+namespace VikingQuiz.Api.Mappers
+{
+    public class UserMapper : IEntityMapper<User, UserViewModel>
+    {
+
+    }
+}
