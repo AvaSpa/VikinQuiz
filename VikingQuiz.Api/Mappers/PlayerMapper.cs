5581a4b5a89fb8c8f976f6eb4facea7abcaa05df
WIRTEK\louisa.vasies <louisavasies74@gmail.com>
2018-07-19 11:12:12 +0300

    Added Player Mapper and View

---
+++ VikingQuiz.Api/Mappers/PlayerMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VikingQuiz.Api.Models;
+using VikingQuiz.Api.ViewModels;
+using VikingQuiz.Api.Mappers;
+
+namespace VikingQuiz.Api.Mappers
+{
+    public class PlayerToViewMapper : IEntityMapper<Player, PlayerViewModel>
+    {
+        public PlayerViewModel Map(Player player)
+        {
+            var result = new PlayerViewModel
+            {
+                PictureUrl = player.PictureUrl,
+                Name = player.Name
+            };
+            return result;
+        }
+    }
+
+    public class PlayerViewModelToPlayerMapper : IEntityMapper<PlayerViewModel, Player>
+    {
+        public Player Map(PlayerViewModel playerViewModel)
+        {
+            var result = new Player
+            {
+                PictureUrl = playerViewModel.PictureUrl,
+                Name = playerViewModel.Name
+            };
+            return result;
+        }
+    }
+}
---
+++ VikingQuiz.Api/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VikingQuiz.Api.ViewModels
+{
+    public class PlayerViewModel
+    {
+        public string PictureUrl { get; set; }
+
+        public string Name { get; set; }
+    }
+}
