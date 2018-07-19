d4374e9b9d7066b36d5235e5fb2b39cf856ce06d
WIRTEK\louisa.vasies <louisavasies74@gmail.com>
2018-07-19 11:18:14 +0300

    Added Question Model and View

---
+++ VikingQuiz.Api/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VikingQuiz.Api.ViewModels
+{
+    public class PlayerViewModel
+    {
+        public string Text { get; set; }
+
+        public int CorrectAndId { get; set; }
+    }
+}
