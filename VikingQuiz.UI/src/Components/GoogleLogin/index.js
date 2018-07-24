4d9d7d69ec42ae7df5248d67d6c7a130ff1e1bc3
Silviu Gherman <sil.gherman@gmail.com>
2018-07-24 09:24:03 +0300

    Updated the GoogleLogin Button prototype

--- VikingQuiz.UI/src/Components/GoogleLogin/index.js
+++ VikingQuiz.UI/src/Components/GoogleLogin/index.js
@@ -1,71 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import GoogleLogin from 'react-google-login';
 import axios from 'axios';
+import './bootstrap.min.css';
+import './index.css';
+import './fontello-embedded.css';
 
-class GoogleMessage {
-   constructor(googleResObj) { // name, email, pictureUrl
-      this.name = googleResObj.w3.ig;
-      this.email = googleResObj.w3.U3;
-      this.picturUrl = googleResObj.w3.Paa;
+class GoogleMessage { // data format for sending the back-end data
+   constructor(googleResObj) {
+      this.name = googleResObj.w3.ig || null;
+      this.email = googleResObj.w3.U3 || null;
+      this.picturUrl = googleResObj.w3.Paa || null;
    }
 }
 
+// wrapped for the GoogleLogin
 class GoogleLoginButton extends React.Component {
 
    responseGoogle = (response) => {
       var ctx = this;
+
       if( response.hasOwnProperty("error") && response.error === "popup_closed_by_user") {
          this.props.onPopupClosed();
       } 
       else if(response.hasOwnProperty("googleId") ) {
          this.props.onResponseSuccesful();
-         
-         axios.post({
-            method: "POST",
-            url: this.props.postURL,
-            data: new GoogleMessage(response)
-         })
-            .then(function(res) {
-               ctx.props.onPostRequestRecieved(res);
-            })
-            .catch(function(error) {
-               ctx.props.onPostRequestRecieved(error);
-            });
+
+         axios.post(
+            ctx.props.postURL, 
+            new GoogleMessage(response)
+         )
+            .then(ctx.props.onPostSuccess) // post succesful
+            .catch(ctx.props.onPostSuccess); // post failure
       }
-   }
-   request = (response) => {
-      this.props.onPopupOpen();
    }
 
    render() {
       return (
-         <div>
+         <div className={this.props.containerClassName}>
             <GoogleLogin
-               clientId="973616639194-in3pvi0r75qp73f0d92m034r0nq71iqm.apps.googleusercontent.com"
-               buttonText="Login"
+               style={{}}
+               className={this.props.btnClassName}
+               clientId={this.props.clientId}
+               buttonText={<i className="icon-google"></i>}
+
                onSuccess={this.responseGoogle}
                onFailure={this.responseGoogle}
-               onRequest={this.request}
+               onRequest={this.props.onPopupOpen}
             />
          </div>
       );
    }
 }
 
+export default GoogleLoginButton;
 
-ReactDOM.render(
-   (
-      // rendering the Google Login Button with different arguments
-      <GoogleLoginButton
-         onPopupClosed={() => { console.log("onPopupClosed"); }}// onPopupClosed()
-         onResponseSuccesful={() => { console.log("onResponseSuccesful"); }}// onResponseSuccesful()
-         onPostRequestRecieved={() => { console.log("onPostRequestRecieved (or failed)"); }}// onPostRequestRecieved()
-         onPopupOpen={() => { console.log("onPopupOpen"); }}// onPopupOpen()
+/* Example of using this component */
+// ReactDOM.render(
+//    (
+//       <GoogleLoginButton
+//          clientId="973616639194-in3pvi0r75qp73f0d92m034r0nq71iqm.apps.googleusercontent.com"
+//          onPopupClosed={ () => { console.log("popup closed"); } }
+//          onResponseSuccesful={ () => { console.log("response succesful"); } }
+//          onPostError={ () => { console.log("post request failed"); } }
+//          onPostSuccess={ () => { console.log("post request successful"); } }
+//          onPopupOpen={ () => { console.log("popup open"); } }
 
-         postURL={"http://www.google.com/"}
-
-      />
-   ),
-   document.getElementById('root'));
+//          postURL={"http://localhost:8080/"}
+//          btnClassName="btn btn-primary"
+//          containerClassName="social-button google-button"
+//       />
+//    ),
+//    document.getElementById('root')
+// );
