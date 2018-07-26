510c76a363f9a4e91c412c36bb843c5941db94df
WIRTEK\andrei.abrudan <andiabrudan@yahoo.com>
2018-07-26 14:57:46 +0300

    Added facebook and google buttons to SignUpPage

--- VikingQuiz.UI/src/Components/SignUpPage/SignUpPage.tsx
+++ VikingQuiz.UI/src/Components/SignUpPage/SignUpPage.tsx
@@ -5,6 +5,18 @@ import InputData from '../../entities/InputData';
 import HomeButton from '../Buttons/HomeButton/HomeButton';
 import SubmitButton from '../Buttons/SubmitButton/SubmitButton';
 import LoginButton from '../Buttons/LoginSignUpButtons/LoginButton';
+import SocialButtonsWrapper from '../socialButtons/socialButtonsWrapper';
+
+
+function popupClosedHandler(): void { console.log("Popup closed"); }
+function popupOpenHandler(): void { console.log("Popup opened"); }
+ 
+function postSuccesful(res: any): void { console.dir("Post succesful", res); }
+function postError(res: any): void { console.dir("Post NOT succesful", res); }
+ 
+function responseSuccesfulHandler(res: any): void { console.log("Response succesful", res); }
+function responseFailureHandler(): void { console.dir("Response failed"); }
+
 
 class SignUpPage extends React.Component<any, any> {
   constructor(props: any) {
@@ -19,14 +31,14 @@ class SignUpPage extends React.Component<any, any> {
                 <LoginButton />
                 <HomeButton />
                 <div className="row">
-                    <div className="col-md-4 col-md-offset-4">
+                    <div className="col-md-6 col-md-offset-3">
                         <div className="signupmsg">
                             SIGN UP
                         </div>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-4 col-md-offset-4">
+                    <div className="col-md-6 col-md-offset-3">
                         <div className="form-container">
                             <FormComponent inputs={[
                                 new InputData('user-name', 'text', 'Name', ''),
@@ -38,7 +50,26 @@ class SignUpPage extends React.Component<any, any> {
                                 <SubmitButton />
                             </div>
                             <div className="socials">
-                                SIGN UP WITH
+                                <SocialButtonsWrapper 
+                                    postURLs={{
+                                        facebook: 'http://localhost:8080/',
+                                        google: 'http://localhost:8080/'
+                                    }}
+                                    clientIds={{
+                                        facebook: "426789224472011",
+                                        google: "973616639194-in3pvi0r75qp73f0d92m034r0nq71iqm.apps.googleusercontent.com"
+                                    }}
+                                    wrapperMessage={"Sign Up Using"}
+                            
+                                    onResponseFailure={responseFailureHandler}
+                                    onResponseSuccesful={responseSuccesfulHandler}
+                            
+                                    onPopupClosed={popupClosedHandler}
+                                    onPopupOpen={popupOpenHandler}
+                            
+                                    onPostError={postError}
+                                    onPostSuccess={postSuccesful}
+                                />
                             </div>
                         </div>
                     </div>
@@ -51,3 +82,46 @@ class SignUpPage extends React.Component<any, any> {
 }
 
 export default SignUpPage;
+
+
+/*
+import SocialButtonsWrapper from './components/socialButtons/socialButtonsWrapper';
+ 
+function popupClosedHandler(): void { console.log("Popup closed"); }
+function popupOpenHandler(): void { console.log("Popup opened"); }
+ 
+function postSuccesful(res: any): void { console.dir("Post succesful", res); }
+function postError(res: any): void { console.dir("Post NOT succesful", res); }
+ 
+function responseSuccesfulHandler(res: any): void { console.log("Response succesful", res); }
+function responseFailureHandler(): void { console.dir("Response failed"); }
+ 
+ 
+ 
+ReactDOM.render(
+   (
+      <SocialButtonsWrapper
+         postURLs={{
+            facebook: 'http://localhost:8080/',
+            google: 'http://localhost:8080/'
+         }}
+         clientIds={{
+            facebook: "426789224472011",
+            google: "973616639194-in3pvi0r75qp73f0d92m034r0nq71iqm.apps.googleusercontent.com"
+         }}
+         wrapperMessage={"Sign Up Using"}
+ 
+         onResponseFailure={responseFailureHandler}
+         onResponseSuccesful={responseSuccesfulHandler}
+ 
+         onPopupClosed={popupClosedHandler}
+         onPopupOpen={popupOpenHandler}
+ 
+         onPostError={postError}
+         onPostSuccess={postSuccesful}
+ 
+      />
+ 
+   ),
+   document.getElementById('root'));
+*/
