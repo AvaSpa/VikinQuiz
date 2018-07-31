e4154eccd4e1569136843e1ba0c4ed1330735886
WIRTEK\louisa.vasies <louisavasies74@gmail.com>
2018-07-31 15:02:17 +0300

    Added Red Border for Error

--- VikingQuiz.UI/src/Components/FormComponent/FormInput/FormInput.tsx
+++ VikingQuiz.UI/src/Components/FormComponent/FormInput/FormInput.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import './FormInput.css';
 
 const formInput: any = (props: any) => {
+    let className = 'myinput form-control';
+    if (props.ErrorMessage !== ""){
+      className += ' my-error';
+    }
     return (
        <div className="formgroup col-xs-12">
         <label className="formlabel" htmlFor={props.InputId}>{props.InputLabel}</label>
-        <input id={props.InputId} name={props.name} className="myinput form-control" type={props.InputType} onChange={props.changed}/>
+        <input id={props.InputId} name={props.name} className={className} type={props.InputType} onChange={props.changed}/>
         <div className="formerror">{props.ErrorMessage}</div>
       </div>
     )
