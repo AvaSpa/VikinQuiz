e83a4bd46917c6ee3950947eda5f12fbcc0063ec
WIRTEK\louisa.vasies <louisavasies74@gmail.com>
2018-08-01 12:19:26 +0300

    Added Code Review Changes

--- VikingQuiz.UI/src/Components/FormComponent/FormInput/FormInput.tsx
+++ VikingQuiz.UI/src/Components/FormComponent/FormInput/FormInput.tsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 import './FormInput.css';
 
 const formInput: any = (props: any) => {
-    let className = 'myinput form-control';
-    if (props.ErrorMessage !== ""){
-      className += ' my-error';
-    }
+  const defaultInputClass = 'vq-input form-control';
+  const errorInputClass = props.ErrorMessage ? ' input-error' : '';
     return (
        <div className="formgroup col-xs-12">
-        <label className="formlabel" htmlFor={props.InputId}>{props.InputLabel}</label>
-        <input id={props.InputId} name={props.name} className={className} type={props.InputType} onChange={props.changed}/>
+            <label className="formlabel" htmlFor={props.InputId}>{props.InputLabel}</label>
+            <input id={props.InputId} name={props.name} className={defaultInputClass + errorInputClass} type={props.InputType} onChange={props.changed} />
         <div className="formerror">{props.ErrorMessage}</div>
       </div>
     )
