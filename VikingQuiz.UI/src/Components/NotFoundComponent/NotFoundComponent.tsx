14434a735cc2d1003fa9baf6c2ee1dfe8be31d32
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-08-01 11:30:06 +0300

    Add NotFoundComponent

--- VikingQuiz.UI/src/Components/NotFoundComponent/NotFoundComponent.tsx
+++ VikingQuiz.UI/src/Components/NotFoundComponent/NotFoundComponent.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
+import BottomLogo from '../BottomLogo/BottomLogo'
 import './NotFoundComponent.css';
 
-const NotFoundComponent: any = (props: any) => {
+const notFoundComponent: any = (props: any) => {
     return (
        <div className="not-found">
           <div id="not-found-image" />
           <h1 className="very-large">grrr...</h1>
           <p className="large">we're angry too that this doesn't work</p>
           <p className="small">now let's get back to serious business'</p>
+          <footer><BottomLogo /></footer>
       </div>
     )
 }
 
-export default NotFoundComponent;
+export default notFoundComponent;
