b8a2200cc6233aa71876f75b97fb73101456a06f
WIRTEK\louisa.vasies <louisavasies74@gmail.com>
2018-08-01 12:38:27 +0300

    Start Game Page

---
+++ VikingQuiz.UI/src/Components/StartGame/StartGame.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import './StartGame.css';
+import HomeButton from '../Buttons/HomeButton/HomeButton';
+import BottomLogo from '../BottomLogo/BottomLogo';
+
+class StartGame extends React.Component<any, any> {
+    constructor(props: any) {
+      super(props);
+    }
+
+
+
+    public render() {
+        const randomstring = require("randomstring");
+        const code = randomstring.generate({
+            length: 6,
+            charset: 'abcdefghijklmnopqrstuvwxyz0123456789'
+        });
+        return (
+            <div className="codeForm">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                            <HomeButton/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                            <div className="codeLabel"> YOUR CODE </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                            <div className="code"> {code} </div>
+                        </div>
+                    </div>
+                </div>
+                <footer id="footer"><BottomLogo /></footer>  
+            </div>
+        ); 
+      }
+}
+
+export default StartGame;
