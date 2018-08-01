4ead6c0cf5965d4d8ada45717f1c6e5aad94827a
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-08-01 11:21:59 +0300

    Refactor App.tsx and add not-found.png

--- VikingQuiz.UI/src/App.tsx
+++ VikingQuiz.UI/src/App.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import './App.css';
 import SignUpPage from 'src/Components/SignUpPage/SignUpPage';
+<<<<<<< Updated upstream
 import { BrowserRouter , Route, Switch } from "react-router-dom";
 import LoginPage from './Components/LoginPage/LoginPage';
 import RedirectComponent from './Components/RedirectComponent/RedirectComponent';
 import NotFoundComponent from './Components/NotFoundComponent/NotFoundComponent';
-import BottomLogo from './Components/BottomLogo/BottomLogo';
+=======
+import { BrowserRouter , Route } from "react-router-dom";
+import LoginPage from './Components/LoginPage/LoginPage';
+
+>>>>>>> Stashed changes
 
 class App extends React.Component<{}, {}> {
   constructor(props: any) {
@@ -14,20 +19,23 @@ class App extends React.Component<{}, {}> {
 
   public render() {
     return (
-      <>
       <BrowserRouter>
         <div className="App">
-          <Switch>
-            <Route exact={true} path="/" component={SignUpPage} />
-            <Route exact={true} path="/signup" component={SignUpPage} />
-            <Route exact={true} path="/login" component={LoginPage} />
-           <Route exact={true} path="/redirect" component={RedirectComponent} />
-            <Route path="*" component={NotFoundComponent} />
+<<<<<<< Updated upstream
+        <Switch>
+          <Route exact={true} path="/" component={SignUpPage} />
+          <Route exact={true} path="/signup" component={SignUpPage} />
+          <Route exact={true} path="/login" component={LoginPage} />
+          <Route exact={true} path="/redirect" component={RedirectComponent} />
+          <Route path="*" component={NotFoundComponent} />
           </Switch>
+=======
+          <Route exact path="/" component={SignUpPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/login" component={LoginPage} />
+>>>>>>> Stashed changes
         </div>
       </BrowserRouter>
-      <footer><BottomLogo /></footer>
-      </>
     );
   }
 }
