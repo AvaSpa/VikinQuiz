c818d3fe8b4da827963229b9a414b3fa0441043d
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-08-01 11:21:59 +0300

    Refactor App.tsx and add not-found.png

--- VikingQuiz.UI/src/App.tsx
+++ VikingQuiz.UI/src/App.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import './App.css';
 import SignUpPage from 'src/Components/SignUpPage/SignUpPage';
+<<<<<<< Updated upstream
 import { BrowserRouter , Route, Switch } from "react-router-dom";
 import LoginPage from './Components/LoginPage/LoginPage';
 import RedirectComponent from './Components/RedirectComponent/RedirectComponent';
 import NotFoundComponent from './Components/NotFoundComponent/NotFoundComponent';
+=======
+import { BrowserRouter , Route } from "react-router-dom";
+import LoginPage from './Components/LoginPage/LoginPage';
+
+>>>>>>> Stashed changes
 
 class App extends React.Component<{}, {}> {
   constructor(props: any) {
@@ -15,6 +21,7 @@ class App extends React.Component<{}, {}> {
     return (
       <BrowserRouter>
         <div className="App">
+<<<<<<< Updated upstream
         <Switch>
           <Route exact={true} path="/" component={SignUpPage} />
           <Route exact={true} path="/signup" component={SignUpPage} />
@@ -22,6 +29,11 @@ class App extends React.Component<{}, {}> {
           <Route exact={true} path="/redirect" component={RedirectComponent} />
           <Route path="*" component={NotFoundComponent} />
           </Switch>
+=======
+          <Route exact path="/" component={SignUpPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/login" component={LoginPage} />
+>>>>>>> Stashed changes
         </div>
       </BrowserRouter>
     );
