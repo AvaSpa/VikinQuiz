023d8f761236db9de364bdd4dac33a45a9f9331e
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-08-02 10:45:50 +0300

    Refactor NotFoundComponent, FormInput and App Components: - Move footer to App.tsx - Refactor NotFoundComponent (capitalize name and delete foooter) - Refactor FormInput (capitalize name) - Remove footer from existing components (Login, Signup)

--- VikingQuiz.UI/src/Components/FormComponent/FormInput/FormInput.tsx
+++ VikingQuiz.UI/src/Components/FormComponent/FormInput/FormInput.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import './FormInput.css';
 
-const formInput: any = (props: any) => {
-  const defaultInputClass = 'vq-input form-control';
-  const errorInputClass = props.ErrorMessage ? ' input-error' : '';
+const FormInput: any = (props: any) => {
     return (
        <div className="formgroup col-xs-12">
-            <label className="formlabel" htmlFor={props.InputId}>{props.InputLabel}</label>
-            <input id={props.InputId} name={props.name} className={defaultInputClass + errorInputClass} type={props.InputType} onChange={props.changed} />
+        <label className="formlabel" htmlFor={props.InputId}>{props.InputLabel}</label>
+        <input id={props.InputId} name={props.name} className="myinput form-control" type={props.InputType} onChange={props.changed}/>
         <div className="formerror">{props.ErrorMessage}</div>
       </div>
     )
