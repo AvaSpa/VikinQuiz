d2ef035ca8a6bc9e420362f3b1515a938a1d3c9f
Slitthe <sil.gherman@gmail.com>
2018-08-03 11:49:52 +0300

    Modified Quizzes Controller and Quiz repo to also verifiy the user id

--- VikingQuiz.Api/Repositories/QuizRepository.cs
+++ VikingQuiz.Api/Repositories/QuizRepository.cs
@@ -32,15 +32,29 @@ namespace VikingQuiz.Api.Repositories
             return quiz;
         }
 
-        public void DeleteQuiz(int id)
+        public Quiz DeleteQuiz (int id, int userId)
         {
-            var games = context.Game.Where(g => g.QuizId == id).ToList();
-            context.Game.RemoveRange(games);
-            var quizquestions = context.QuizQuestion.Where(qq => qq.QuizId == id).ToList();
-            context.QuizQuestion.RemoveRange(quizquestions);
-            var quizToRemove = context.Quiz.FirstOrDefault(q => q.Id == id);
-            context.Quiz.Remove(quizToRemove);
-            context.SaveChanges();
+            // check if the player has acces to that quiz in order to delete it
+            Quiz quiz = new Quiz
+            {
+                Id = id
+            };
+
+            
+            var quizMatchingIdAndPlayerId = ctx.Quiz.Where(x => x.Id== id && x.UserId == userId).ToList();
+            if( quizMatchingIdAndPlayerId.Count == 0 )
+            {
+                return null;
+            }
+
+            var games = ctx.Game.Where(x => x.QuizId == id).ToList();
+            ctx.Game.RemoveRange(games);
+            var quizquestions = ctx.QuizQuestion.Where(x => x.QuizId == id).ToList();
+            ctx.QuizQuestion.RemoveRange(quizquestions);
+            ctx.Quiz.Remove(quiz);
+            ctx.SaveChanges();
+
+            return quizMatchingIdAndPlayerId.FirstOrDefault();
         }
 
         public Quiz GetQuizById(int id)
@@ -52,15 +66,10 @@ namespace VikingQuiz.Api.Repositories
             return foundQuiz;
         }
 
-        public IEnumerable<Quiz> GetQuizByUserId(int id)
+        public IEnumerable<Quiz> GetAll(int userId)
         {
-            return context.Quiz.Where(q => q.UserId == id).OrderByDescending(q => q.LastModified).ToList();
-
-        }
-
-        public IEnumerable<Quiz> GetAll()
-        {
-            return context.Quiz.ToList();
+            var quizzesOfTheUser = ctx.Quiz.Where(x => x.UserId == userId).ToList();
+            return quizzesOfTheUser;
         }
     }
 }
