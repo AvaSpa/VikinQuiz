0e0c452155fbca685ef27a80047bda40bb4d31a1
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-08-03 15:56:12 +0300

    Delete submit button and make components read-only

---
+++ VikingQuiz.UI/src/Components/QuestionsComponent/MainQuestionComponent/MainQuestionComponent.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './MainQuestionComponent.css';
+
+class MainQuestionComponent extends React.Component {
+
+    public render(){
+        return (
+            <div className='main-question'>
+                <h1>Main question comes here</h1>
+            </div>
+        )
+    }
+}
+
+export default MainQuestionComponent;
