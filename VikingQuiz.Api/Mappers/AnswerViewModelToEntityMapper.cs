68acfd0e26250906179b1e8e2373552d06b4ea0b
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-08-06 17:52:51 +0300

    Refactor Mappers and QuestionController: - Mappers: AnswerMapper and QuestionMapper

---
+++ VikingQuiz.Api/Mappers/AnswerViewModelToEntityMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VikingQuiz.Api.Models;
+using VikingQuiz.Api.ViewModels;
+
+namespace VikingQuiz.Api.Mappers
+{
+    public class AnswerViewModelToEntityMapper : IEntityMapper<AnswerViewModel, Answer>
+    {
+        public Answer Map(AnswerViewModel entity)
+        {
+            var result = new Answer
+            {
+                Id = entity.Id,
+                Text = entity.Text
+            };
+
+            return result;
+        }
+    }
+}
