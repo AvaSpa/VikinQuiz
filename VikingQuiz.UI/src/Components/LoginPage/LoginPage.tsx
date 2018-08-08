5fe430af37e3d53b50ace27ae83a464598550874
WIRTEK\andrei.abrudan <andiabrudan@yahoo.com>
2018-08-08 11:24:15 +0300

    Helped Dragos with server error messages

--- VikingQuiz.UI/src/Components/LoginPage/LoginPage.tsx
+++ VikingQuiz.UI/src/Components/LoginPage/LoginPage.tsx
@@ -4,7 +4,6 @@ import SignUpButton from '../Buttons/LoginSignUpButtons/SignUpButton';
 import HomeButton from '../Buttons/HomeButton/HomeButton';
 import SocialButtonsWrapper from '../socialButtons/socialButtonsWrapper';
 import LoginFormComponent from '../LoginFormComponent/LoginFormComponent';
-import { setTimeout } from 'timers';
 import { Redirect } from 'react-router-dom';
 import { loginRules} from '../../entities/Validation/rules';
 
@@ -21,37 +20,67 @@ function postError(res: any): void { console.dir("Post NOT succesful", res); }
 function responseSuccesfulHandler(res: any): void { console.log("Response succesful", res); }
 function responseFailureHandler(): void { console.dir("Response failed"); }
 
-interface ILoginCredentials{
-    email: string,
-    password: string
-};
+
+
 
 class LoginPage extends React.Component<any, any> {
     private httpService: any = new HttpService();
     private storageService: StorageService = new StorageService();
-    private readonly apiAddress: string = 'http://localhost:60151/api';
-    private readonly apiSessionAddress: string = '/session';
-    private readonly apiGoogleAddress: string = '/google';
-    private readonly apiFacebookAddress: string = '/facebook';
-
+    
     constructor(props: any) {
-        super(props);
+      super(props);
 
-        this.state = {
-            serverMessage: '',
-            redirect: false,
-            checkbox: false
+      this.state = {
+          showErrorMessage: false,
+          serverErrorMessage: '',
+          redirect: false
+      }
+    }
+  
+   
+  
+    public userDataHandler = (url: string, formData: any) => {
+        if(!url || !formData){
+            return;
         }
+
+        const body: any = {
+            email: formData.Email,
+            password: formData.Password 
+        }
+    
+        this.httpService.post(url, body)
+            .then((result: any) => { this.loginSuccess(result); })
+            .catch((error: any) => { this.loginError(error); });
     }
 
-    public componentDidMount() {
-        const remember: boolean = this.storageService.getItem('remember') === 'true' ? true : false;
-        const tokenExists: boolean = this.storageService.itemExists('token');
+    public loginSuccess = (result: any) => {
+        const loginToken: string = result.data.token;
+        this.storageService.saveItem('token', loginToken);
+        this.setState({
+            redirect: true
+        });
+    };
 
-        if (remember && tokenExists) {
-            this.setState({ redirect: true });
+    public loginError = (error: any) => {
+        this.setState({ showErrorMessage: true });
+        if (error.response === undefined) {
+            this.setState({ serverErrorMessage: "Could not connect to the server. Please try again later" });
         }
-    }
+        else {
+            if (error.response.status === 404) {
+                this.setState({ serverErrorMessage: "Username or Password incorrect. Please try again" });
+            }
+            if (error.response.status === 400) {
+                this.setState({ serverErrorMessage: "Please confirm your account first" });
+            }
+        }
+        setTimeout(() => this.setState({
+            showErrorMessage: false,
+            serverErrorMessage: ''
+        }), 5000);
+    };
+
 
     public render() {
       if(this.state.redirect){
@@ -64,7 +93,7 @@ class LoginPage extends React.Component<any, any> {
                   <HomeButton />
                   <div className="row">
                     <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
-                          <div className="login-msg">
+                          <div className="loginmsg">
                               LOG IN
                           </div>
                       </div>
@@ -72,20 +101,19 @@ class LoginPage extends React.Component<any, any> {
                    <div className="row">
                         <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
                           <div className="form-container">
-                              <p className="form-error server-message">{this.state.serverMessage}</p>
+                              {this.state.showErrorMessage ? (<div className="message server-message">{this.state.serverErrorMessage}</div>) : null}
                               <LoginFormComponent inputs={[
                                 {id: 'user-email', type: 'email', label: 'Email', errorMessage: '', name: 'Email', value: ''},
                                 {id: 'user-password', type: 'password', label: 'Password', errorMessage: '', name: 'Password', value: ''},
-                                ]} url={this.apiAddress + this.apiSessionAddress} buttonName="" onSubmit={this.userDataHandler} 
+                                ]} url="http://localhost:60151/api/session" buttonName="" onSubmit={this.userDataHandler} 
                            validator={loginValidator}
                            validationRules={loginRules}
-                           checkboxChangedHandle={this.checkboxClickedHandle}
                                 />
                               <div className="socials">
                               <SocialButtonsWrapper 
                                     postURLs={{
-                                        facebook:   this.apiAddress + this.apiFacebookAddress,
-                                        google:     this.apiAddress + this.apiGoogleAddress
+                                        facebook: 'http://localhost:60151/api/facebook',
+                                        google: 'http://localhost:60151/api/google'
                                     }}
                                     clientIds={{
                                         facebook: "1691716487610141",
@@ -110,48 +138,6 @@ class LoginPage extends React.Component<any, any> {
           </div>   
       );
     }
-    
-    private userDataHandler = (url: string, formData: any) => {
-        if (!url || !formData) {
-            return;
-        }
-
-        const body: ILoginCredentials = {
-            email: formData.Email,
-            password: formData.Password
-        }
-
-        this.httpService.post(this.apiAddress + this.apiSessionAddress, body)
-        .then((result: any) => this.onLogInSuccess(result))
-        .catch((error: any) => this.onLogInError(error))
-    };
-
-    private onLogInSuccess = (result: any) => {
-        const loginToken: string = result.data.token;
-        this.storageService.saveItem('token', loginToken);
-        this.setState({redirect: true});
-    };
-
-    private onLogInError = (error: any) => {
-        if (!error) {
-            this.setState({
-                serverMessage: "Could not connect to the server"
-            });
-            return;
-        };
-        this.setState({
-            serverMessage: error.response.data
-        })
-        setTimeout(() => this.setState({
-            serverMessage: ''
-        }), 5000);
-    };
-
-    private checkboxClickedHandle = () => {
-        const checkboxChange: boolean = !this.state.checkbox;
-        this.setState({ checkbox: checkboxChange });
-        this.storageService.saveItem('remember', checkboxChange.toString());
-    };
   }
   
   export default LoginPage;
--- VikingQuiz.UI/src/services/StorageService.tsx
+++ VikingQuiz.UI/src/services/StorageService.tsx
@@ -7,10 +7,11 @@ class StorageService{
     public getItem = (key: string) : string | null => {
         return localStorage.getItem(key);
     };
+    
+    public removeItem = (key: string) => {
+        localStorage.removeItem(key);
+    };
 
-    public itemExists = (key: string) : boolean => {
-        return localStorage.hasOwnProperty(key);
-    }
 }
 
 export default StorageService;
