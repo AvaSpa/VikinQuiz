b55f8252b9a0af53a1435c83631764f6d0e171f4
WIRTEK\dragos.dumitrescu <dumitrescudragos03@gmail.com>
2018-08-09 12:08:42 +0300

    Refactor Backend QuizController and QuestionController to use Token Note: fix validation on model in Entity Framework

--- VikingQuiz.Api/ViewModels/QuestionViewModel.cs
+++ VikingQuiz.Api/ViewModels/QuestionViewModel.cs
@@ -1,8 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace VikingQuiz.Api.ViewModels
 {
@@ -10,10 +6,8 @@ namespace VikingQuiz.Api.ViewModels
     {
 
         public int Id { get; set; }
-        [Required(ErrorMessage = "Question's text can not be empty")]
-        [StringLength(256, ErrorMessage = "Question's text should not be longer than 256 characters")]
+        
         public string Text { get; set; }
-        [Required(ErrorMessage = "Questions should have a correct answer")]
         public int CorrectAnswerId { get; set; }
         public List<AnswerViewModel> Answers { get; set; }
     }
