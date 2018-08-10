e935faf0f06edefd5d46ef3010af6af95fad5109
Silviu Gherman <sil.gherman@gmail.com>
2018-08-10 13:11:46 +0300

    Redesign My profile

--- VikingQuiz.UI/src/Components/ProfileAndHomeComponent/ProfileAndHomeComponent.tsx
+++ VikingQuiz.UI/src/Components/ProfileAndHomeComponent/ProfileAndHomeComponent.tsx
@@ -3,18 +3,15 @@ import './ProfileAndHomeComponent.css'
 import HomeButton from '../Buttons/HomeButton/HomeButton';
 import MyProfileComponent from '../MyProfileComponent/MyProfileComponent';
 
-interface IProfileComponent {
-    profilePictureUrl: string,
-    profileName: string
-}
-
-const ProfileAndHomeComponent = (props: IProfileComponent) => {
+const ProfileAndHomeComponent = (props: any) => {
 
     return(
         <div className="profile-container">
             <HomeButton />
             <div className="profile-details">
-                <MyProfileComponent profilePictureUrl={props.profilePictureUrl} profileName={props.profileName}/>
+                <MyProfileComponent profilePictureUrl={props.profilePictureUrl} profileName={props.profileName}>
+                  {props.children}
+                </MyProfileComponent>
             </div>
         </div>
     )
