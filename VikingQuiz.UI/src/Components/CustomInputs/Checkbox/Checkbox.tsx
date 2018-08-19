e4a7817844a851e047cce21708dc0526e413dd9b
Silviu Gherman <sil.gherman@gmail.com>
2018-08-19 13:33:36 +0300

    Made the login checkbox be the same as the choose answer checkbox

--- VikingQuiz.UI/src/Components/CustomInputs/Checkbox/Checkbox.tsx
+++ VikingQuiz.UI/src/Components/CustomInputs/Checkbox/Checkbox.tsx
@@ -2,13 +2,28 @@ import * as React from 'react';
 import './Checkbox.css';
 
 const CustomCheckbox: any = (props: any) => {
+    let labelClassList = ["custom-checkbox"]
+    if (props.labelClassNames) {
+        labelClassList = labelClassList.concat(props.labelClassNames);
+    }
 
-   return (
-      <label className="custom-checkbox">
-         <input type="checkbox" name={props.name} checked={props.checked} onChange={props.changeHandler}/>
-         <span className="checkbox-display" />
-      </label>
-   )
+    let displayClassList = ["checkbox-display"]
+    if (props.displayClassNames) {
+        displayClassList = displayClassList.concat(props.displayClassNames);
+    }
+
+    let displayContainerClassList = ["checkbox-display-container"]
+    if (props.displayContainerClassNames) {
+        displayContainerClassList = displayClassList.concat(props.displayContainerClassList);
+    }
+
+    return <label className={labelClassList.join(" ")} htmlFor={props.inputId || null}>
+        <input id={props.inputId || null} type="checkbox" name="test" checked={props.checked} onChange={props.changeHandler} {...props.inputOptions} />
+        {props.labelText || ""}
+        <div className={displayContainerClassList.join(" ")}>
+            <span className={displayClassList.join(" ")} />
+        </div>
+    </label>;
 }
 
 export default CustomCheckbox;
