d2349297ba00a6944088c40fa227a2753e8b2b82
Silviu Gherman <sil.gherman@gmail.com>
2018-08-20 15:24:49 +0300

    Modified the Azure Storage Service to be more general
    	-- modified the actual Azure Storage Service to accept a
    	container name upon instantiation
    	-- added a method which returns the url endpoint for a container
    	-- added a controller to get a full azure url based on a
    	container name
    	-- added the ability to get the answers images using the newly
    	created services and controller

---
+++ VikingQuiz.Api/Controllers/StorageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VikingQuiz.Api.Models;
+using VikingQuiz.Api.Repositories;
+using VikingQuiz.Api.ViewModels;
+using VikingQuiz.Api.Mappers;
+using VikingQuiz.Api.Utilities;
+using Microsoft.WindowsAzure.Storage;
+
+namespace VikingQuiz.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class StorageController : Controller
+    {
+        [HttpGet("{containerName}")]
+        public IActionResult GetUrlOfContainer(string containerName)
+        {
+            return Ok( AzureBlobService.GetFullUrlOfContainer(containerName) );
+        }
+
+
+    }
+}
+
